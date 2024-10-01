cec41b70a81c5f98634666a4925013f012f6c0ba
Vikash <yadav621gzp@gmail.com>
2024-10-02 02:40:13 +0530

    Smart contract deployed on NeoX

--- SmartContract/hardhat.config.js
+++ SmartContract/hardhat.config.js
@@ -1,17 +1,25 @@
 
 require ('@nomiclabs/hardhat-waffle');
 
+task("accounts","Prints the list of the accounts",async (taskArgs , hre )=>{
+  const accounts = await hre.ethers.getSigners();
+
+  for(const account of accounts){
+    console.log(account.address);
+  }
+})
 
 module.exports = {
   solidity: "0.8.10",
 
-  defaultNetwork: "Edu",
+  defaultNetwork: "NeoTestnet",
   networks:{
     hardhat:{},
-    Edu: {
-      url: "https://rpc.open-campus-codex.gelato.digital/",
-      chainId: 656476,
-      accounts: ['a77637fc7905e02b1ec4b9e913cc895b627be58053f2fdd774911623004c3285'],
+    NeoTestnet: {
+      gasPrice: 40000000000,
+      url: "https://neoxt4seed1.ngd.network",
+      chainId: 12227332,
+      accounts: ['5753e65f56865a161fbf41932a0d855139a4ce9dc20d82fb655bff393fc41702'],
     },
   }
 };
