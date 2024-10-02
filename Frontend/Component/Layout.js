920aff50a790d6c851805e0c662ae247998211de
Vikash <yadav621gzp@gmail.com>
2024-10-02 19:18:42 +0530

    Removed the un necessary files

--- Frontend/Component/Layout.js
+++ Frontend/Component/Layout.js
@@ -1,13 +1,8 @@
 
 
-import Hero from "./v1.0.0/hero/Hero";
-import About from "./v1.0.0/about/About";
-import Problem from "./v1.0.0/problemsolved/Problem";
 import Footer from "./v1.0.0/footer/footer";
 
-import HappyClient from "./v1.0.0/happyClient/HappyClient";
-import Explore from "./v1.0.0/Explore/Explore";
-import Card from "../Component/v1.0.0/Cards/Cards"
+
 
  import Navbar from "./Course/Nav";
 export default function Layout({ children }) {
@@ -19,8 +14,6 @@ export default function Layout({ children }) {
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         <Navbar/>
-        {/* Below commented component only works if Navbar is imported in _app.js || To be done so once ever component listed below is complete */}
-        {/* <Navbar handleLogout={handleLogout}/> */} 
 
         {children}
 
